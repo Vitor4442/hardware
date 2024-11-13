85322802e666b870531ebaa29687b520fc276636
Vitor4442 <jose.vitorw018@gmail.com>
2024-11-13 14:59:48 -0300

    Add files via upload

--- sketch_oct29a/sketch_oct29a.ino
+++ sketch_oct29a/sketch_oct29a.ino
@@ -1,51 +1,36 @@
 #define led 13
 int contador = 0;
-#define tmp 2000
 
 void setup() {
-  // Inicializa a comunicação serial
+
   Serial.begin(19200); 
   pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);  // Garante que o LED inicie desligado
+  digitalWrite(led, LOW); 
 }
 
 void loop() {
-  // Verifica se há dados disponíveis na porta serial
-  if (Serial.available() > 0) {  
-    char c = Serial.read();  // Lê o próximo caractere
-    
-    // Controle do LED: acende se for 'A' ou apaga se for 'a'
-    if (c == 'A') {
-      digitalWrite(led, HIGH);
-    } else if (c == 'a') {
-      digitalWrite(led, LOW);
-    }
 
-    // Imprime o valor ASCII do caractere recebido e seus dígitos
+  if (Serial.available() > 0) {  
+    char c = Serial.read();
+    
+    digitalWrite(led, (c == 'A') ? HIGH : (c == 'a') ? LOW : LOW);
+
     converterASCparadec(c);
     contador++;
   }
-
-  // Parte para testar a comunicação serial e enviar os caracteres 'A' e 'a' com intervalo
-  Serial.write('A'); 
-  delay(tmp);
-  Serial.write('a');  
-  delay(tmp);
 }
 
+
 void converterASCparadec(char c) {
-  // Converte o caractere para seu valor ASCII
+
   int valorASCII = (int)c;
 
-  // Separa o valor ASCII em dezenas e unidades
   int dezena = valorASCII / 10;
   int unidade = valorASCII % 10;
 
-  // Converte os números das dezenas e unidades para caracteres
   char digito1 = '0' + dezena;
   char digito2 = '0' + unidade;
 
-  // Exibe as informações no monitor serial
   Serial.print("Caractere: ");
   Serial.println(c); 
 
