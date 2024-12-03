3589c21b146085e9c864108ae4951eb6372a4365
José Vitor <josev@exemplo.com>
2024-12-02 23:03:56 -0300

    converso de dec para hexa

--- sensor/sketch_nov27a/sketch_nov27a.ino
+++ sensor/sketch_nov27a/sketch_nov27a.ino
@@ -5,6 +5,19 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+void converterASCparadec(int valor, char& digito1, char& digito2, int base) {
+  if (base != 16) {
+    Serial.println("Base inválida! Use apenas base 16.");
+    return;
+  }
+
+  int dezena = valor / base;  
+  int unidade = valor % base; 
+
+  digito1 = (dezena <= 9) ? (dezena + '0') : (dezena - 10 + 'A');
+  digito2 = (unidade <= 9) ? (unidade + '0') : (unidade - 10 + 'A');
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Iniciando leitura do DHT22...");
@@ -15,26 +28,31 @@ void setup() {
 void loop() {
   delay(2000);
 
-  float umidade = dht.readHumidity();
-  
-  float temperatura = dht.readTemperature();
-  
-  float tempFahrenheit = dht.readTemperature(true);
+  int umidade = dht.readHumidity();
+  int temperatura = dht.readTemperature();
 
   if (isnan(umidade) || isnan(temperatura)) {
     Serial.println("Falha ao ler o sensor!");
     return;
   }
 
-  Serial.print("Umidade: ");
-  Serial.print(umidade);
-  Serial.println("%");
+  char umidDig1, umidDig2, tempDig1, tempDig2;
 
-  Serial.print("Temperatura: ");
-  Serial.print(temperatura);
-  Serial.println("°C");
+  converterASCparadec(umidade, umidDig1, umidDig2, 16);
+  converterASCparadec(temperatura, tempDig1, tempDig2, 16);
 
-  Serial.print("Temperatura: ");
-  Serial.print(tempFahrenheit);
-  Serial.println("°F");
+
+
+  Serial.write("Umidade em Hexadecimal: ");
+  Serial.write(umidDig1);
+  Serial.write(umidDig2);
+  Serial.write('\n'); 
+
+
+
+
+  Serial.write("Temperatura em Hexadecimal: ");
+  Serial.write(tempDig1);
+  Serial.write(tempDig2);
+  Serial.write('\n'); 
 }
