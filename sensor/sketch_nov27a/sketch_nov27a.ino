172ae29bc50eac10ea70e96c9a0473006f819654
José Vitor <josev@exemplo.com>
2024-12-02 23:13:02 -0300

    funcao de imprimir

--- sensor/sketch_nov27a/sketch_nov27a.ino
+++ sensor/sketch_nov27a/sketch_nov27a.ino
@@ -5,6 +5,7 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+
 void converterASCparadec(int valor, char& digito1, char& digito2, int base) {
   if (base != 16) {
     Serial.println("Base inválida! Use apenas base 16.");
@@ -16,6 +17,25 @@ void converterASCparadec(int valor, char& digito1, char& digito2, int base) {
 
   digito1 = (dezena <= 9) ? (dezena + '0') : (dezena - 10 + 'A');
   digito2 = (unidade <= 9) ? (unidade + '0') : (unidade - 10 + 'A');
+}
+
+void imprimir(int umidade, int temperatura) {
+  
+  char umidDig1, umidDig2;
+  char tempDig1, tempDig2;
+
+  converterASCparadec(umidade, umidDig1, umidDig2, 16);
+  converterASCparadec(temperatura, tempDig1, tempDig2, 16);
+
+  Serial.print("Umidade em Hexadecimal: ");
+  Serial.write(umidDig1);
+  Serial.write(umidDig2);
+  Serial.write('\n'); 
+
+  Serial.print("Temperatura em Hexadecimal: ");
+  Serial.write(tempDig1);
+  Serial.write(tempDig2);
+  Serial.write('\n'); 
 }
 
 void setup() {
@@ -36,23 +56,6 @@ void loop() {
     return;
   }
 
-  char umidDig1, umidDig2, tempDig1, tempDig2;
-
-  converterASCparadec(umidade, umidDig1, umidDig2, 16);
-  converterASCparadec(temperatura, tempDig1, tempDig2, 16);
-
-
-
-  Serial.write("Umidade em Hexadecimal: ");
-  Serial.write(umidDig1);
-  Serial.write(umidDig2);
-  Serial.write('\n'); 
-
-
-
-
-  Serial.write("Temperatura em Hexadecimal: ");
-  Serial.write(tempDig1);
-  Serial.write(tempDig2);
-  Serial.write('\n'); 
+ 
+  imprimir(umidade, temperatura);
 }
