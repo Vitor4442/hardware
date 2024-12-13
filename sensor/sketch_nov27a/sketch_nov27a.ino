597642ebde2b712889e520ec5df0b257842ff69d
José Vitor <josev@exemplo.com>
2024-12-12 21:51:07 -0300

    controle de estado e comunicação serial

--- sensor/sketch_nov27a/sketch_nov27a.ino
+++ sensor/sketch_nov27a/sketch_nov27a.ino
@@ -1,12 +1,16 @@
 #include <DHT.h>
-
-#define MAX_REGISTROS 10
 #define DHTPIN 2
 #define DHTTYPE DHT22
+#define ESTADO_1 1
+#define ESTADO_2 2
+#define ESTADO_3 3
+#define ESTADO_4 4
 
 DHT dht(DHTPIN, DHTTYPE);
-char dados[MAX_REGISTROS][4]; 
+char dados[6];
 int indice = 0;
+int contador = 0;
+int ControlEstado = 0;
 
 void converterASCparadec(int valor, char& digito1, char& digito2, int base) {
   if (base != 16) {
@@ -21,34 +25,57 @@ void converterASCparadec(int valor, char& digito1, char& digito2, int base) {
   digito2 = (unidade <= 9) ? (unidade + '0') : (unidade - 10 + 'A');
 }
 
-void imprimir(int umidade, int temperatura) {
+void processarMensagem(char dado) {
   char umidDig1, umidDig2;
   char tempDig1, tempDig2;
+  int umidade = dht.readHumidity();
+  int temperatura = dht.readTemperature();
+
+  if (isnan(umidade) || isnan(temperatura)) {
+    Serial.println("Falha ao ler o sensor!");
+    return;
+  }
 
   converterASCparadec(umidade, umidDig1, umidDig2, 16);
   converterASCparadec(temperatura, tempDig1, tempDig2, 16);
 
+  char header1 = 'A';
+  char header2 = 'B';
 
-  dados[indice][0] = umidDig1;
-  dados[indice][1] = umidDig2;
-  dados[indice][2] = tempDig1;
-  dados[indice][3] = tempDig2;
-
-  indice = (indice + 1) % MAX_REGISTROS;
-
-  Serial.println("\n=== Dados Armazenados ===");
-  for (int i = 0; i < MAX_REGISTROS; i++) {
-    Serial.print("Registro ");
-    Serial.print(i);
-    Serial.print(": Umidade= ");
-    Serial.write(dados[i][0]);
-    Serial.write(dados[i][1]);
-    Serial.print(", Temperatura= ");
-    Serial.write(dados[i][2]);
-    Serial.write(dados[i][3]);
-    Serial.println();
+  switch (dado) {
+    case 't':
+      Serial.println("Enviando temperatura:");
+      Serial.write(header1);
+      Serial.write(header2);
+      Serial.write(tempDig1);
+      Serial.write(tempDig2);
+      break;
+    case 'u':
+      Serial.println("Enviando umidade:");
+      Serial.write(header1);
+      Serial.write(header2);
+      Serial.write(umidDig1);
+      Serial.write(umidDig2);
+      break;
+    case 'a':
+      Serial.println("Enviando td:");
+      Serial.write(header1);
+      Serial.write(header2);
+      Serial.write(umidDig1);
+      Serial.write(umidDig2);
+      Serial.write(tempDig1);
+      Serial.write(tempDig2);
+      break;
+    case 'c':
+      Serial.println("Enviando contador:");
+      Serial.write(header1);
+      Serial.write(header2);
+      Serial.print(contador);
+      break;
+    default:
+      Serial.println("Comando inválido!");
+      break;
   }
-  Serial.println("=========================\n");
 }
 
 void setup() {
@@ -58,39 +85,26 @@ void setup() {
 }
 
 void loop() {
-  delay(2000); 
-
-  int umidade = dht.readHumidity();
-  int temperatura = dht.readTemperature();
-
-  if (isnan(umidade) || isnan(temperatura)) {
-    Serial.println("Falha ao ler o sensor!");
-    return;
-  }
-
-  imprimir(umidade, temperatura);
-
   if (Serial.available()) {
-    char comando = Serial.read();
+    char msg_tipo = Serial.read();
 
-    switch (comando) {
-      case 'T':
-        Serial.print("EB 90 Temperatura:");
-        Serial.write(dados[indice - 1][2]);
-        Serial.write(dados[indice - 1][3]);
-        Serial.println();
+    switch (ControlEstado) {
+      case ESTADO_1:
+        ControlEstado = (msg_tipo == 0xEB) ? ESTADO_2 : 0;
         break;
-
-      case 'U':
-        Serial.print("EB 90 umidade: ");
-        Serial.write(dados[indice - 1][0]);
-        Serial.write(dados[indice - 1][1]);
-        Serial.println();
+      case ESTADO_2:
+        ControlEstado = (msg_tipo == 0x90) ? ESTADO_3 : 0;
         break;
-
+      case ESTADO_3:
+        processarMensagem(msg_tipo);
+        ControlEstado = 0;
+        break;
       default:
-        Serial.println("Comando inválido!");
+        ControlEstado = 0;
         break;
     }
   }
+
+  contador++;
+  delay(2000);
 }
--- sensor/sketch_oct17b/sketch_oct17b.ino
+++ sensor/sketch_oct17b/sketch_oct17b.ino
@@ -1,21 +1,74 @@
-#define temp 2000
+#define MAX_REGISTROS 10
+#define TEMP 2000
+int indice = 0;
+char respostas[MAX_REGISTROS][7]; 
+int header1 = 0xEB;
+int header2 = 0x90;
 
-void setup(){
+
+void setup() {
   Serial.begin(9600);
 }
 
-void loop(){
-  Serial.write('T');
-  delay(100);
-  while (Serial.available()){
-    Serial.println(Serial.readString());
+void loop() {
+  
+  enviarComando('t');
+  delay(TEMP);
+  receberResposta();
+
+  enviarComando('u');
+  delay(TEMP);
+  receberResposta();
+
+  enviarComando('a');
+  delay(TEMP);
+  receberResposta();
+
+  enviarComando('c');
+  delay(TEMP);
+  receberResposta();
+
+
+  delay(2000);
 }
 
-  delay(temp);
+void enviarComando(char comando) {
+  Serial.write(header1);
+  Serial.write(header2);
+  Serial.write(comando);
 
-Serial.write('U');
-  delay(100);
-  while (Serial.available()){
-    Serial.println(Serial.readString());
 }
+
+void receberResposta() {
+  if (Serial.available() >= 6) {
+    for (int i = 0; i < 6; i++) {
+      respostas[indice][i] = Serial.read();
+    }
+    if (Serial.available() > 0) {
+      respostas[indice][6] = Serial.read();
+    }
+    indice = (indice + 1) % MAX_REGISTROS;
+  }
+}
+
+void exibirRespostas() {
+  for (int i = 0; i < MAX_REGISTROS; i++) {
+    Serial.print("Registro ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.print("Header1=");
+    Serial.write(respostas[i][0]);
+    Serial.write(respostas[i][1]);
+    Serial.print(" ");
+    Serial.print("Temp=");
+    Serial.write(respostas[i][2]);
+    Serial.write(respostas[i][3]);
+    Serial.print(" ");
+    Serial.print("Umidade=");
+    Serial.write(respostas[i][4]);
+    Serial.write(respostas[i][5]);
+    Serial.print(" ");
+    Serial.print("Contador=");
+    Serial.println(respostas[i][6]);
+  }
 }
